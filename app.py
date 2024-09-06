e7d9e871efcb044b2e655b623a1f88d03c6325d4
Doga CAN <154826159+dogitosanchez@users.noreply.github.com>
2024-09-06 11:20:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def create_plotly_insulin_line_graph():
 
     fig.update_layout(xaxis_title='Year', yaxis_title='Number of cases', 
                       title={'x':0.5, 'xanchor': 'center'},  # Center the title
-                      height=500, width=600, autosize=True )  # Adjust height and width
+                      height=500, autosize=True )  # Adjust height and width
 
     return fig
 
@@ -65,7 +65,7 @@ def create_plotly_line_graph():
 
     fig.update_layout(xaxis_title='Year', yaxis_title='Number of cases', 
                       title={'x':0.5, 'xanchor': 'center'},  # Center the title
-                      height=500, width=600, autosize=True)  # Adjust height and width
+                      height=500, autosize=True)  # Adjust height and width
 
     return fig
 
